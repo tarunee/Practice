42c0218423d33dffbbdb1d95f7c3bd0c6233d106
tarunee <taru.1297@gmail.com>
2018-07-05 02:00:30 +0530

    Create allSubsets.py

---
+++ InterviewBit/Backtracking/allSubsets.py
@@ -0,0 +1,36 @@
+class Solution:
+    # @param A : list of integers
+    # @return a list of list of integers
+    def subsets(self, A):
+        A=sorted(A)
+        l=[]
+        n=len(A)
+        if(n==0):
+            l.append([])
+            return l
+        if(n==1):
+            l.append([])
+            l.append(A)
+            return l
+        
+        B=[]
+        for i in range(1,n):
+            B.append(A[i])
+        
+        fin = self.subsets(B)
+        
+        for i in range(len(fin)):
+            k=[A[0]]+fin[i]
+            l.append(k)
+            
+        if(A[0]<fin[1][0]):
+            for i in range(len(l)):
+                fin.insert(i+1,l[i])
+        else:
+            for i in range(len(l)):
+                fin.append(l[i]);
+        
+        return fin
+        
+        
+        
